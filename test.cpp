906493dd031d2b16ab1891e7a9b332f76ca8337b
JeremyGuo <jeremyguo@pku.edu.cn>
2022-12-09 16:30:38 +0800

    Add Init setting of test

--- test.cpp
+++ test.cpp
@@ -1,6 +1,13 @@
 #include <string>
 #include <cassert>
 
+#include <rte_eal.h>
+#include <rte_memcpy.h>
+#include <rte_ethdev.h>
+
+#include <netinet/ip.h>
+#include <netinet/ether.h>
+
 #include "common.hpp"
 #include "gtest/gtest.h"
 
@@ -10,18 +17,132 @@ static uint32_t N;
 
 #define MAXN 32
 
+class NIC;
+NIC **NICs;
+
+int ip2nic[MAXN];
+
 class NIC{
 public:
-    NIC() {
-        idx = this - NICs;
-    }
+    NIC(int _idx): idx(_idx) {}
 
     void init() {
-        assert(0 && "Not imp");
+        assert(!rte_eth_dev_info_get(idx, &m_devInfo) && "Get Dev info failed");
+        m_conf.link_speeds = 0;
+        m_conf.rxmode = {
+            .mq_mode = ETH_MQ_RX_NONE,
+            .mtu = 1500,
+            .max_lro_pkt_size = 0,
+            .split_hdr_size = 0
+        };
+        m_conf.txmode = {
+            .mq_mode = ETH_MQ_TX_NONE,
+            .offloads =
+                DEV_TX_OFFLOAD_VLAN_INSERT |
+                DEV_TX_OFFLOAD_IPV4_CKSUM  |
+                DEV_TX_OFFLOAD_UDP_CKSUM   |
+                DEV_TX_OFFLOAD_TCP_CKSUM   |
+                DEV_TX_OFFLOAD_SCTP_CKSUM  |
+                DEV_TX_OFFLOAD_TCP_TSO,
+        };
+        m_conf.lpbk_mode = 0;
+        m_conf.rxmode.offloads &= m_devInfo.rx_offload_capa;
+        m_conf.txmode.offloads &= m_devInfo.tx_offload_capa;
+        uint16_t num_rx_desc = 128;
+        uint16_t num_tx_desc = 128;
+        assert(!rte_eth_dev_adjust_nb_rx_tx_desc(idx, &num_rx_desc, &num_tx_desc) && "Failed to set desc number");
+        assert(!rte_eth_dev_configure(idx, 1, 1, &m_conf) && "Failed to configure NIC");
+
+        m_mempool = rte_mempool_create(
+            std::to_string(idx).c_str(),
+            512,
+            1514 + RTE_PKTMBUF_HEADROOM,
+            64,
+            sizeof(struct rte_pktmbuf_pool_private),
+            rte_pktmbuf_pool_init, nullptr,
+            rte_pktmbuf_init, nullptr, 
+            rte_eth_dev_socket_id(idx), 0
+        );
+        assert(m_mempool && "Failed to create mempool");
+        auto rxq_conf = m_devInfo.default_rxconf;
+        rxq_conf.offloads = m_conf.rxmode.offloads;
+        assert(!rte_eth_rx_queue_setup(idx, 0, num_rx_desc, rte_socket_id(), &rxq_conf, m_mempool) && "Failed to setup rx queue");
+
+        auto txq_conf = m_devInfo.default_txconf;
+        txq_conf.offloads = m_conf.txmode.offloads;
+        assert(!rte_eth_tx_queue_setup(idx, 0, num_tx_desc, rte_socket_id(), &txq_conf) && "Failed to setup tx queue");
+
+        assert(!rte_eth_promiscuous_enable(idx) && "Failed to enable promiscuous mode");
+        assert(!rte_eth_dev_start(idx) && "Failed to start NIC");
+    }
+    
+    // buf must large than 1500 bytes
+    int read(char* buf) {
+        if (rx_cnt == rx_read) {
+            for (int i = 0; i < rx_cnt; i ++) {
+                rte_pktmbuf_free(rx_buff[i]);
+            }
+            rx_cnt = rte_eth_rx_burst(idx, 0, rx_buff, 16);
+            rx_read = 0;
+        }
+        if (rx_cnt == rx_read)
+            return 0;
+        auto cur = rx_read;
+        ++rx_read;
+        
+        rte_memcpy(buf, rte_pktmbuf_mtod(rx_buff[cur], char*), rx_buff[cur]->pkt_len);
+        return rx_buff[cur]->pkt_len;
+    }
+
+    int write(const char* buf, int len) {
+        tx_buff[0] = rte_pktmbuf_alloc(m_mempool);
+        assert (!this->tx_buff[0] && "Mempool ran out");
+        tx_buff[0]->pkt_len = tx_buff[0]->data_len = len;
+        tx_buff[0]->nb_segs = 1;
+        tx_buff[0]->next = NULL;
+
+        auto ret = rte_pktmbuf_mtod(tx_buff[0], char*);
+        tx_buff[0]->ol_flags = 0;
+        
+        rte_memcpy(ret, buf, len);
+        assert (rte_eth_tx_burst(idx, 0, tx_buff, 1) == 1 && "Failed to send packet");
+        return 0;
+    }
+
+    void sendIP(const char* src, const char* dst, const char* payload, int len) {
+        char tmp[1600];
+        auto ethh = reinterpret_cast<struct ethhdr*>(tmp);
+        ethh->h_proto = ETH_P_IP;
+
+        auto iph = reinterpret_cast<struct iphdr*>(tmp + sizeof(struct ethhdr));
+        iph->ihl = 5;
+        iph->version = 4;
+        iph->tos = 0;
+        auto tot_len = (sizeof(struct ethhdr) + len + 3) >> 2 << 2;
+        iph->tot_len = htons(tot_len);
+        iph->id = 0;
+        iph->frag_off = 0;
+        iph->ttl = 24;
+        iph->protocol = IPPROTO_TCP;
+        iph->check = 0;
+        iph->saddr = inet_addr(src);
+        iph->daddr = inet_addr(dst);
+
+        this->write(tmp, tot_len + sizeof(struct ethhdr));
     }
 
     int idx;
-} NICs[MAXN];
+
+private:
+    struct rte_eth_dev_info m_devInfo;
+    struct rte_eth_conf m_conf;
+    struct rte_mempool* m_mempool;
+    
+    uint16_t rx_cnt = 0;
+    uint16_t rx_read = 0;
+    struct rte_mbuf* rx_buff[16];
+    struct rte_mbuf* tx_buff[1];
+};
 
 class {
 public:
@@ -105,11 +226,31 @@ int main(int argc, char** argv) {
         fin.close();
         
         /** validate options **/
+        assert(N > 0 && "N should > 0");
         assert(N <= MAXN && "N should <= MAXN (32)");
+
+        /** Init DPDK **/
+        {
+            int margc = 4;
+            char** margv = new char*[margc];
+            std::string argvs[] = {
+                "./main",
+                "-c",
+                "0xff",
+                "--log-level=critical"
+            };
+            for (int i = 0; i < margc; i ++) {
+                margv[i] = (char*)argvs[i].c_str();
+            }
+            rte_eal_init(margc, margv);
+            delete [] margv;
+        }
         
         /** Init local NICs **/
+        NICs = new NIC*[N];
         for (int i = 0; i < N; i ++) {
-            assert(0 && "Not imp");
+            NICs[i] = new NIC(i);
+            NICs[i]->init();
         }
 
         /** Send settings to peer **/
@@ -119,7 +260,22 @@ int main(int argc, char** argv) {
 
         /** Load remote settings **/
         for (int i = 0; i < N; i ++) {
-            assert(0 && "Not imp");
+            NICs[0]->sendIP("0.0.0.0", ("10.1.1." + std::to_string(i + 1)).c_str(),
+                "123123", 6);
+            char ret[1600];
+            bool found = false;
+            while (!found) {
+                for (int j = 0; j < N; j ++) {
+                    auto len = NICs[j]->read(ret);
+                    if (len < 14 + 20 + 6)
+                        continue;
+                    char* ptr = ret + 34;
+                    if (memcmp(ptr, "123123", 6) == 0) {
+                        found = true;
+                        ip2nic[i] = j;
+                    }
+                }
+            }
         }
 
         return RUN_ALL_TESTS();
